e8bbbfb035c0bc1a390127e245b4ca08cf91d64b
karina <karina.popeye@gmail.com>
2021-11-09 14:39:49 -0800

    add style

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-//console.log("Hello World");
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
